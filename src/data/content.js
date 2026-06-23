f4e239cc07fbe84c89a14cef1ec0f35705873abc
kirito02092002-tech <kirito02092002@gmail.com>
2026-06-23 10:26:27 +0100

    Add files via upload

---
+++ src/data/content.js
@@ -0,0 +1,63 @@
+export const GYM_INFO = {
+  name: 'Cactusfit',
+  phone: '25 944 877',
+  phoneSecondary: '50 944 874',
+  phoneFull: '(+216) 25 944 877',
+  phoneDisplay: '25 944 877',
+  phoneTel: '+21625944877',
+  phoneSecondaryTel: '+21650944874',
+  email: 'cactusfit2022@gmail.com',
+  instagram: 'cactus._.fit',
+  instagramUrl: 'https://www.instagram.com/cactus._.fit/',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=100077760826370',
+  mapsUrl: 'https://maps.app.goo.gl/pohNjnd3mu2o7GDY9',
+  coords: { lat: 36.9001083, lng: 10.1808233 },
+  mapsEmbedUrl:
+    'https://www.google.com/maps?q=36.9001083,10.1808233&hl=fr&z=16&output=embed',
+  address: 'Ariana Soghera, Ariana, Tunisie, 2083',
+};
+
+export const PLANNING_COVER = '/images/info/planning-cover.jpg';
+
+export const PLANNING_POSTERS = {
+  monday: '/images/planning/monday.jpg',
+  tuesday: '/images/planning/tuesday.jpg',
+  wednesday: '/images/planning/wednesday.jpg',
+  thursday: '/images/planning/thursday.jpg',
+  friday: '/images/planning/friday.jpg',
+};
+
+export const SCHEDULE_KEYS = [
+  'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+];
+
+export const SCHEDULE = {
+  monday: [{ time: '19H30', name: 'Combat HIT', coach: 'Hela' }],
+  tuesday: [{ time: '19H30', name: 'Pilates', coach: 'Kenza' }],
+  wednesday: [{ time: '19H30', name: 'Total Abdo', coach: 'Malek' }],
+  thursday: [{ time: '19H30', name: 'Circuit Training', coach: 'Med Amine' }],
+  friday: [{ time: '19H30', name: 'Danse Orientale', coach: 'Donia' }],
+};
+
+export const HERO_IMAGES = {
+  main: '/images/gym/hero-alt.jpg',
+  avatars: [
+    '/images/gym/treadmills.jpg',
+    '/images/gym/dumbbells.jpg',
+    '/images/gym/cardio-console.jpg',
+  ],
+};
+
+export const SECTION_IMAGES = {
+  stats: '/images/gym/treadmills.jpg',
+  why: '/images/gym/dumbbells.jpg',
+};
+
+export const SERVICE_ITEMS = [
+  { id: 'group', image: '/images/info/planning-cover.jpg' },
+  { id: 'boxing', image: '/images/planning/monday.jpg' },
+  { id: 'cross', image: '/images/planning/thursday.jpg' },
+  { id: 'smart', image: '/images/gym/hero-main.jpg' },
+  { id: 'bodypump', image: '/images/gym/dumbbells.jpg' },
+  { id: 'cardio', image: '/images/gym/treadmills.jpg' },
+];
